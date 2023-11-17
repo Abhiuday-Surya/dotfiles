179783c990ce44fbc3fe8a46d5f5767a60c22d9a
Abhiuday-Surya <93286245+Abhiuday-Surya@users.noreply.github.com>
2023-11-18 00:41:24 +0530

    Add files via upload

---
+++ lua/pluginlist.lua
@@ -0,0 +1,131 @@
+return {
+
+    { 'numToStr/Comment.nvim', opts = {} },
+
+    {
+        "numToStr/Comment.nvim",
+        config = function()
+            require("Comment").setup()
+        end
+    },
+
+    {
+        "ellisonleao/gruvbox.nvim",
+        priority = 1000,
+        config = function()
+            vim.cmd("colorscheme gruvbox")
+        end
+    },
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
+    "neovim/nvim-lspconfig",
+
+    'folke/neodev.nvim', -- new
+
+    {
+        'hrsh7th/nvim-cmp',
+        dependencies = {
+            'L3MON4D3/LuaSnip',
+            'saadparwaiz1/cmp_luasnip',
+            'rafamadriz/friendly-snippets',
+            'hrsh7th/cmp-nvim-lsp',
+        },
+    },
+
+    {
+        'nvim-treesitter/nvim-treesitter',
+        build = ':TSUpdate',
+    },
+
+
+    { 
+        'nvim-telescope/telescope.nvim',
+        dependencies = { 'nvim-lua/plenary.nvim' } 
+    },
+
+    {
+        'nvim-telescope/telescope-fzf-native.nvim',
+        build = 'make',
+    },
+
+
+    {
+        'nvim-lualine/lualine.nvim',
+        dependencies = {
+            "nvim-tree/nvim-web-devicons"
+        },
+        config = function()
+            require("lualine").setup({
+                icons_enabled = true,
+                theme = 'gruvbox',
+            })
+        end,
+    },
+	"nvim-tree/nvim-tree.lua",
+	{
+		'goolord/alpha-nvim',
+		dependencies = { 'nvim-tree/nvim-web-devicons' },
+		config = function ()
+			require'alpha'.setup(require'alpha.themes.startify'.config)
+		end
+	},
+	{'romgrk/barbar.nvim',
+		dependencies = {
+			'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
+			'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
+		},
+		init = function() vim.g.barbar_auto_setup = false end,
+		opts = {
+			-- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
+			-- animation = true,
+			-- insert_at_start = true,
+			-- …etc.
+		},
+		version = '^1.0.0', -- optional: only update when a new 1.x version is released
+	},
+	{
+		'stevearc/dressing.nvim',
+		opts = {},
+	},
+	{
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		init = function()
+			vim.o.timeout = true
+			vim.o.timeoutlen = 300
+		end,
+		opts = {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+		}
+	},
+	{
+		"rcarriga/nvim-notify",
+		config = function ()
+			vim.notify = require("notify")
+		end
+	},
+	{
+	   'akinsho/toggleterm.nvim',
+		version = "*",
+		config = true
+	},
+	{
+		"aurum77/live-server.nvim",
+		config = function()
+			require("plugins.live-server")
+		end,
+		cmd = {
+			"LiveServer",
+			"LiveServerStart",
+			"LiveServerStop",
+			"LiveServerInstall",
+		},
+		build = function()
+			require("live_server.util").install()
+		end,
+	},
+	"folke/tokyonight.nvim",
+	"shaunsingh/nord.nvim"
+}
